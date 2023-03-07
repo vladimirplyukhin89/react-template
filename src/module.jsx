765cf38d5912849270ee18a5b53062e7f06cf71e
Pluhin <Pluhin@itpc.ru>
2023-03-07 17:00:29 +0500

    Homemade React template

---
+++ src/module.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+import book from "./img/book.png"
+
+export const Module = () => {
+  return (
+    <>
+      <h1>Hello, React</h1>
+      <img width={400} src={book} alt="book" />
+    </>
+  )
+}
